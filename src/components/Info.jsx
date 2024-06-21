318122538b1e32b3e28263beb53a34464ea941b7
henrytrust1111 <henrytrust1111@gmail.com>
2024-06-21 11:59:46 +0100

    aboutUs4

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,19 +1,69 @@
-import React from 'react'
-import {personalInfo} from '../data'
+import React, { useState, useEffect } from 'react';
+import { personalInfo } from '../data';
 
 const Info = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 700) {
+        setShowAll(true);
+      } else {
+        setShowAll(false);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    setShowAll(!showAll);
+  };
+
+  const displayedInfo = showAll ? personalInfo : personalInfo.slice(0, 3);
+
   return (
     <>
-    {personalInfo.map(({title, description}, index) => {
-        return (
-          <li className="info__item" key={index}>
-            <span className="info__title">{title}</span>
-            <span className="info__description">{description}</span>
-          </li>
-        )
-    })}
+      {displayedInfo.map(({ title, description }, index) => (
+        <li className="info__item" key={index}>
+          <span className="info__title">{title}</span>
+          <span className="info__description">{description}</span>
+        </li>
+      ))}
+      <button onClick={handleToggle} className="info__toggle">
+        {showAll ? 'View Less' : 'View All'}
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
+
+
+
+
+
+// import React from 'react'
+// import {personalInfo} from '../data'
+
+// const Info = () => {
+//   return (
+//     <>
+//     {personalInfo.map(({title, description}, index) => {
+//         return (
+//           <li className="info__item" key={index}>
+//             <span className="info__title">{title}</span>
+//             <span className="info__description">{description}</span>
+//           </li>
+//         )
+//     })}
+//     </>
+//   )
+// }
+
+// export default Info
