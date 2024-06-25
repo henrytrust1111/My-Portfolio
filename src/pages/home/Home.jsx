07f2a3b99d7e3389e2c34ec32de8104a19b3b7ee
henrytrust1111 <henrytrust1111@gmail.com>
2024-06-25 20:09:57 +0100

    Portfolio5

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           <p className="home__description">
             I'm a Nigerian based web designer & front-end developer focused on
             crafting clean & user-friendly experiences, I am passionate about
-            building excellent softwares the improves the lives of those around
+            building excellent softwares that improves the lives of those around
             me.
           </p>
 
